45137b48216ac79b595a53518911b478771d341b
kexin-Star <89749795+kexin-Star@users.noreply.github.com>
2022-05-22 17:38:05 -0400

    chatt front end

---
+++ lab1/swiftChatter/swiftChatter/swiftChatter/ChattTableCell.swift
@@ -0,0 +1,15 @@
+//
+//  ChattTableCell.swift
+//  swiftChatter
+//
+//  Created by kexin on 5/22/22.
+//
+
+import UIKit
+
+final class ChattTableCell: UITableViewCell {
+
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var timestampLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+}
